35f0c3b9749ede4acff63bed5ede79e393a08f24
Ryan Abney <ryan@rcabney.com>
2018-02-16 19:17:52 -0500

    added first 25 questions

---
+++ billy.js
@@ -0,0 +1,164 @@
+const myQuestions = [
+    {
+      question: "What is Billy's maid's name?",
+      answers: {
+        a: "Jaunita",
+        b: "Agnes",
+        c: "Consuela"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "What does Scotty tell Billy he had for breakfast?",
+      answers: {
+        a: "Eggs",
+        b: "Beans",
+        c: "Burritos"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "Who would Billy rather do?",
+      answers: {
+        a: "Jack Nicholson (now)",
+        b: "Jack Nicholson (1974)",
+        c: "Meg Ryan"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Who wins the pickle race?",
+      answers: {
+        a: "Frank",
+        b: "Billy",
+        c: "Jack"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "What is the name of the song Billy dances to after he comes up with the idea to go back to school?",
+      answers: {
+        a: "Karma Chameleon",
+        b: "Electric Avenue",
+        c: "I'll Tumble For You"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "What is Karl Alphonse's job title at Madison Hotel?",
+      answers: {
+        a: "Marketing Director",
+        b: "Senior Vice President",
+        c: "Operations Manager"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "What is the name of Billy's favorite nudey magazine?",
+      answers: {
+        a: "Women Over 80",
+        b: "She-male Fiesta",
+        c: "Drunk Chicks"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "What did Jaunita pack in Billy's lunch instead of a snack pack?",
+      answers: {
+        a: "apple",
+        b: "banana",
+        c: "orange"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "What is Billy's dad's name?",
+      answers: {
+        a: "William",
+        b: "Edward",
+        c: "Brian"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "What kind of crackers did Karl have in the car on the way to the Madison's house?",
+      answers: {
+        a: "Triscuit",
+        b: "Saltines",
+        c: ""
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "Which of the following did not write the film?",
+      answers: {
+        a: "Adam Sandler",
+        b: "Robert Smigel",
+        c: "Tim Herlihy"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "What is the name of Billy's elementary school?",
+      answers: {
+        a: "Knibb Elementary",
+        b: "Ben Franklin Elementary",
+        c: "Polly Fector Elementary"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "What is Eric Gordon's job title at Madison Hotels?",
+      answers: {
+        a: "Chief Operating Officer",
+        b: "Executive Vice President",
+        c: "Chairman of the Board"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "How many employees work for Madison Hotel?",
+      answers: {
+        a: "61,000",
+        b: "5,000",
+        c: "25,000"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "What is the name of the puppy in 'The Puppy who lost his Way'?",
+      answers: {
+        a: "Skippy",
+        b: "Happy",
+        c: "Friendly"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "What date is Billy's deadline to all 12 grades?",
+      answers: {
+        a: "June 15",
+        b: "May 15",
+        c: "July 15"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "What is the name of Billy's third grade friend?",
+      answers: {
+        a: "Ernie",
+        b: "Michael",
+        c: "Steven"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "What is Old Man Clemen's wife's name?",
+      answers: {
+        a: "Barbara",
+        b: "Emily",
+        c: "Veronica"
+      },
+      correctAnswer: "a"
+    }
+  ];
