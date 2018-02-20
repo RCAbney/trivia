c58fa79a71927313beed219f10c64c94b60c9caa
Ryan Abney <ryan@rcabney.com>
2018-02-19 21:02:49 -0500

    add more questions

--- billy.js
+++ billy.js
@@ -63,6 +63,179 @@ const myQuestions = [
       correctAnswer: "c"
     },
     {
+        question: "How does Jaunita offer to cheer Billy up?",
+        answers: {
+            a: "She asks him if he wants a snack pack",
+            b: "She offers to take her shirt off for him",
+            c: "She lets him skip school for the day"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What sport does Billy play in fifth grade",
+        answers: {
+            a: "basketball",
+            b: "dodgeball",
+            c: "track and field"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "What color is Miss Lippy's car?",
+        answers: {
+            a: "red",
+            b: "yellow",
+            c: "green"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What does Miss Vaughn catch Billy doing while he is daydreaming?",
+        answers: {
+            a: "Drawing a picture of her that says 'do me Billy'",
+            b: "Pretending he's singing a song with her",
+            c: "He's making out with a picture of her"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Which word did Billy not write in cursive?",
+        answers: {
+            a: "Dugout",
+            b: "Bunt",
+            c: "Buzz"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Who does Eric coerce into stopping Billy?",
+        answers: {
+            a: "Frank",
+            b: "The Bus Driver",
+            c: "Principal Anderson"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Which is NOT a flavor on Billy's Slurpee machine?",
+        answers: {
+            a: "Raspberry",
+            b: "Coca-Cola",
+            c: "Orange"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "What page does Miss Vaughn tell her class to open their books to?",
+        answers: {
+            a: "45",
+            b: "92",
+            c: "69"
+        },
+        correctAnswer: "c"
+    },
+    
+    {
+        question: "What band's t-shirt is Billy wearing when he arrives at high school for the first time?",
+        answers: {
+            a: "Journey",
+            b: "REO Speedwagon",
+            c: "Styx"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "If peeing your pants is cool, consider me _________",
+        answers: {
+            a: "Robert Redford",
+            b: "Miles Davis",
+            c: "Jack Nicholson"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "If Billy stays home from school, what must he help Jaunita do?",
+        answers: {
+            a: "Shave her armpits",
+            b: "Wax her upper lip",
+            c: "Clip her toenails"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "What is the name of the classmate that Billy phones to apologize to?",
+        answers: {
+            a: "Bob Simonds",
+            b: "Perry Blake",
+            c: "Danny McGrath"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "After Billy is bullied in the high school cafe, what do the other nerds say he has a case of?",
+        answers: {
+            a: "Loser denial",
+            b: "Lack of popularity",
+            c: "Being a total loser"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "How many words of A Tale of Two Cities can Billy understand?",
+        answers: {
+            a: "Four",
+            b: "Eleven",
+            c: "Nine"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What did the Revolting Blob do in June, 1983",
+        answers: {
+            a: "Sat on a guy's head and killed him",
+            b: "Broke a guy's arm",
+            c: "Knocked a guy unconscious with a steel chair"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "According to the lunch lady, how do the kid's like their sloppy joe's?",
+        answers: {
+            a: "Extra meaty",
+            b: "Extra sloppy",
+            c: "Nice and messy"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "How much money did Principal Anderson claim Billy bribed him with?",
+        answers: {
+            a: "$10,000",
+            b: "$5,000",
+            c: "$25,000"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What actor played Billy's high school science teacher?",
+        answers: {
+            a: "Kevin Nealon",
+            b: "David Spade",
+            c: "Robert Smigel"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What story are the 3rd graders reading from their reading is fun book?",
+        answers: {
+            a: "The Puppy Who Lost His Way",
+            b: "Superfudge",
+            c: "My Sister Fanny"
+        },
+        correctAnswer: "c"
+    },
+    
+    {
       question: "What is the name of Billy's favorite nudey magazine?",
       answers: {
         a: "Women Over 80",
@@ -143,6 +316,7 @@ const myQuestions = [
       },
       correctAnswer: "a"
     },
+    
     {
       question: "True or False: Billy has one week to finish each grade.",
       answers: {
